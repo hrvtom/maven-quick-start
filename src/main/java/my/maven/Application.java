ad25861f6fe2b56c4672a72b6f6a7748b7de8118
Hrvoje Tominovic <hrvoje.tominovic@ericsson.com>
2017-03-29 15:24:35 +0200

    Use StringUtils

--- src/main/java/my/maven/Application.java
+++ src/main/java/my/maven/Application.java
@@ -2,6 +2,7 @@ package my.maven;
 
 	import java.util.ArrayList;
 	import java.util.List;
+	import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 	
@@ -9,6 +10,11 @@ public class Application {
     public Application() {
         System.out.println ("Inside Application");
     }
+	
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words, ' ');
+		return ( separateWords == null ) ? 0 : separateWords.length;
+	}
 	
 	public void greet(){
 		List<String> greetings = new ArrayList<>();
@@ -24,5 +30,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
 	app.greet();
+	int count = app.countWords("Some fun words fart shit");
+	System.out.println("Word count "+count);
     }
 }
